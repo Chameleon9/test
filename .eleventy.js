fd5c716571a2e3f11e085b725b877d1697ac8d59
Chameleon9 <95360861+Chameleon9@users.noreply.github.com>
2021-12-16 09:34:43 +0530

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,15 +1,8 @@
 module.exports = function (eleventyConfig){
-
     return{
-
-        dir:{
-
-            input: "src"
-
-            output:"public"
-
+        dir{            
+            input: "src",
+            output:"public",
         },
-
     };
-
 };
